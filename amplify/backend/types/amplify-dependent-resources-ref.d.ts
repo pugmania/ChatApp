6dec3bee37f533674c64b79f038c07b7c3b646cc
Colton Herschberger <herschbergercs@eckerd.edu>
2023-12-27 18:03:44 -0500

    xxx

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -18,11 +18,5 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
-  },
-  "storage": {
-    "s3chatappstorage": {
-      "BucketName": "string",
-      "Region": "string"
-    }
   }
 }
