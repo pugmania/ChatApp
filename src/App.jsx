f0485b42c4632a8fc34447aa073a7e72c9db9c06
Colton Herschberger <herschbergercs@eckerd.edu>
2023-12-28 12:31:17 -0500

    redo image backgrounbd url logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import amplifyconfig from './amplifyconfiguration.json';
 import '@aws-amplify/ui-react/styles.css';
 import './App.css'
+import { list, getUrl } from 'aws-amplify/storage';
 
 Amplify.configure(amplifyconfig);
 function App() {
+  useEffect(() => {
+    function listImages() {
+      list('photos/')
+        .then(result => {
+          result.forEach(async (photo) => {
+            getUrl(photo)
+            console.log(photo)
+          });
+        })
+        .catch(err => console.log(err));
+    }
+    listImages();
+  } , [])
   return <Authenticator>
           {({ signOut, user }) => (
             <main>
