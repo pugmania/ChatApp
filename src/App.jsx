306cd1db710bc884fd95460a0dc74034fb9cb547
Colton Herschberger <herschbergercs@eckerd.edu>
2023-12-29 12:25:09 -0500

    reset to pprevious working instance

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,141 @@
-import { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react';
 import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
-import amplifyconfig from './amplifyconfiguration.json';
+import { Authenticator, Flex, TextField } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import './App.css'
-import { list, getUrl } from 'aws-amplify/storage';
+import './App.css';
+import { generateClient } from "aws-amplify/api";
+import { mutations, queries } from './graphql';
+import awsmobile from './aws-exports';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  Conversation
+} from "@chatscope/chat-ui-kit-react";
+import { list } from 'aws-amplify/storage';
 
-Amplify.configure(amplifyconfig);
-function App() {
+
+const client = generateClient()
+Amplify.configure(awsmobile);
+
+export default function App() {
+  const [backgroundImage, setBackgroundImage] = useState(''); // [1
+  const [message, setMessage] = useState('');
+  const [chatrooms, setChatrooms] = useState([]);
+  const [chats, setChats] = useState([]);
+
+
+
   useEffect(() => {
-    function listImages() {
-      list('photos/')
-        .then(result => {
-          console.log(result)
-          result.forEach(async (photo) => {
-            getUrl(photo)
-            console.log(photo)
-          });
-        })
-        .catch(err => console.log(err));
+    async function updateBackgroundImage() {
+      const imageName = await getRandomImage();
+      if (imageName) {
+        const url = await Storage.get(imageName, { level: 'public' });
+        setBackgroundImage(url);
+      }
     }
-    listImages();
-  } , [])
-  return <Authenticator>
-          {({ signOut, user }) => (
-            <main>
-              <h1>Hello {user.username}</h1>
-              <h1>Hello {user.userId}</h1>
-              {console.log(user)}
-              <button onClick={signOut}>Sign out</button>
-            </main>
-          )}
-        </Authenticator>
-}
+    updateBackgroundImage();
+  }, [])
+  
+  useEffect( () => {
+    client.graphql({
+      query: queries.listChatrooms,
+      variables: {
+          input: {
+            "Chats": [],
+            "title": "Lorem ipsum dolor sit amet"
+      }}
+    })
+    .then((result) => {
+      setChatrooms(result.data.listChatrooms.items);
+      console.log(result);
+    })
+  }, [])
 
-export default App
+  const listImages = async () => {
+    try {
+      const result = await list('public/photos/', { level: 'public' });
+      return result.map(file => file.key);
+    } catch (error) {
+      console.error('Error fetching images from S3', error);
+      return [];
+    }
+  }
+  
+  const getRandomImage = async () => {
+    const images = await listImages();
+    if (images.length === 0) return null;
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+  }
+
+  const sendChat = () => {
+    if (!message.trim()) return; // Prevent sending empty messages
+
+    try {
+      // Replace 'createChat' with the appropriate mutation from your GraphQL schema
+      const chatData = { content: message }; // Adjust based on your schema
+      client.graphql({ query: mutations.createChat, variables: { input: chatData } })
+      .then(() => {setMessage('')})
+      } catch (err) {console.log('error sending chat:', err);}
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      sendChat();
+    }
+  }
+  return  <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <header className="App-header">
+              <h1>Chat App</h1>
+            </header>
+            <Authenticator loginMechanisms={['email', 'username']} socialProviders={['google']}>
+              {({ signOut, user }) => (
+                <main>
+                  <h2>Hello {user.username}</h2>
+                  <p>Choose a chatroom below</p>
+                    <MainContainer>
+                      {chatrooms &&
+                        chatrooms.map((chatroom) => (
+                          <Conversation
+                            key={chatroom.id}
+                            name={chatroom.title}
+                            lastSenderName={chatroom.owner}
+                            info={chatroom.createdAt}
+                            unreadCnt={0}
+                            active
+                          />
+                      ))}
+                      <ChatContainer>
+                        <MessageList>
+                          {chats &&
+                            chats.map((chat) => (
+                              <Message
+                                key={chat.id}
+                                model={{
+                                  message: chat.content,
+                                  sentTime: chat.createdAt,
+                                  sender: chat.owner,
+                                }}
+                              />
+                          ))}
+                          {<Message
+                            model={{
+                              message: "Hello my friend",
+                              sentTime: "just now",
+                              sender: "Joe",
+                            }}
+                          />}
+                        </MessageList>
+                        <MessageInput placeholder="Type message here" />
+                      </ChatContainer>
+                    </MainContainer>            
+                  <button onClick={() => sendChat()}>Send</button>
+                  <button onClick={() => signOut()}>Sign out</button>
+                </main>
+              )}
+            </Authenticator>
+          </div>
+}
---
+++ src/graphql/index.js
@@ -0,0 +1,4 @@
+import * as queries from './queries';
+import * as mutations from './mutations';
+import * as subscriptions from './subscriptions';
+export { queries, mutations, subscriptions };
